09a3bedd9a9f87f021640e980b9cab6d2dd75103
Amal Sudama <cds.sudama@gmail.com>
2021-02-16 18:27:33 -0500

    Test Plants

---
+++ test/plant.test.js
@@ -0,0 +1,18 @@
+const { PlantUMLDeactivate: Subject } = require('../src/node/plant');
+
+describe('PlantUMLDeactivate', () => {
+  let node, puml;
+
+  beforeAll(() => {
+    node = 'NODE';
+    puml = new Subject(node);
+  });
+
+  test('it is defined', () => {
+    expect(Subject).toBeDefined();
+  });
+
+  test('renders', () => {
+    expect(puml.render()).toBe('deactivate "NODE"');
+  });
+});
