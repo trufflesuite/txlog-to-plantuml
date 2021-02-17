c66fc7f19015ed09ef3e33d3214e6fb4c299149b
Amal Sudama <cds.sudama@gmail.com>
2021-02-17 03:25:50 -0500

    Support alias lookup based on address
      - this is needed by selfdestruct and transfer
      - hard code display-name and alias for Externally Owned Account that
        initializes transaction

--- src/actors.js
+++ src/actors.js
@@ -2,19 +2,20 @@ module.exports = class Actors {
   constructor(option) {
     this.shortParticipantNames = option.shortParticipantNames || false;
 
-    this.addresses = new Set();
+    this.addressKeyPairs = new Set();
 
-    this.address2entry = {}; // address -> { address, displayName }
+    // address:contractName -> { address, contractName, alias, displayName }
+    this.addressKey2entry = {};
     this.alias2address = {};
   }
 
   add({address, contractName}, isEOA=false) {
     const key = `${address}:${contractName}`;
-    if (this.addresses.has(key)) {
+    if (this.addressKeyPairs.has(key)) {
       return;
     }
 
-    this.addresses.add(key);
+    this.addressKeyPairs.add(key);
 
     // * alias is used for describing an actor in the plantuml
     // * displayName is how the alias is displayed in participant section
@@ -23,8 +24,7 @@ module.exports = class Actors {
     let alias;
     let displayName;
     if (isEOA) {
-      alias = 'EOA';
-      displayName = this.shortParticipantNames ? alias : address;
+      alias = displayName = 'EOA';
       contractName = 'Externally Owned Account'
     } else {
       alias = `${contractName}_${(++count).toString().padStart(2, 0)}`;
@@ -35,7 +35,7 @@ module.exports = class Actors {
     }
 
     this.alias2address[alias] = key;
-    this.address2entry[key] = {
+    this.addressKey2entry[key] = {
       address,
       alias,
       contractName,
@@ -45,14 +45,23 @@ module.exports = class Actors {
 
   getAlias({address, contractName}) {
     const key = `${address}:${contractName}`;
-    const entry = this.address2entry[key];
+    const entry = this.addressKey2entry[key];
     return entry.alias;
+  }
+
+  getFirstAliasForAddress(address) {
+    for (const key of this.addressKeyPairs) {
+      if (key.startsWith(address)) {
+        return this.addressKey2entry[key].alias;
+      }
+    }
+    return address;
   }
 
   getAllParticipants() {
     const actors = [];
-    for (const ady of this.addresses) {
-      actors.push(this.address2entry[ady]);
+    for (const ady of this.addressKeyPairs) {
+      actors.push(this.addressKey2entry[ady]);
     }
     return actors;
   }
