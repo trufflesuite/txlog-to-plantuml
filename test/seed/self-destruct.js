31482a7bb4f420ce0caf1e26e7e8e6fbecc16c53
Amal Sudama <cds.sudama@gmail.com>
2021-03-02 12:00:01 -0500

    test: add self-destruct expectations

---
+++ test/seed/self-destruct.js
@@ -0,0 +1,51 @@
+const BigNumber = require("bignumber.js");
+
+const txlog = 
+{
+  type: 'transaction',
+  actions: [
+    {
+      type: 'callexternal',
+      address: '0xF1352ccBAd660699Dbc38157C78b7fc43f24F601',
+      contextHash: '0xa4b09519965394df62c5af181dd3606d6296a84bf7b1b9b575c2478e05c66d50',
+      value: BigNumber("0x0"),
+      kind: 'constructor',
+      isDelegate: undefined,
+      functionName: undefined,
+      contractName: 'StacktraceTest',
+      arguments: [
+        {
+          name: 'x',
+          value: {
+            type: { typeClass: 'uint', bits: 256, typeHint: 'uint256' },
+            kind: 'value',
+            value: { asBN: BigNumber("0x1"), rawAsBN: BigNumber("0x1") }
+          }
+        }
+      ],
+      actions: [
+        {
+          type: 'callexternal',
+          address: '0x87a091ED2D2C53C624F8bDdE08019ee1e097569C',
+          contextHash: '0x848192bbbfc979c0bb348ff77b46bc0dc9b01b27d80135943446c4ad4a8c89a5',
+          value: BigNumber("0x0"),
+          kind: 'constructor',
+          isDelegate: undefined,
+          functionName: undefined,
+          contractName: 'PakSau',
+          arguments: [],
+          actions: [],
+          salt: null,
+          returnKind: 'return',
+          returnImmutables: []
+        }
+      ],
+      salt: null,
+      returnKind: 'selfdestruct',
+      beneficiary: '0x8DE97402A58e95A52E045B31fDC2d07CdeFf3946'
+    }
+  ],
+  origin: '0x8DE97402A58e95A52E045B31fDC2d07CdeFf3946'
+}
+
+module.exports = txlog;
---
+++ test/self-destruct.test.js
@@ -0,0 +1,56 @@
+const { visit } = require("../src/trace");
+const Actors = require("../src/actors");
+jest.mock("../src/debugger");
+const {
+  CallRelation: PCall,
+  ReturnRelation: PReturn,
+  RevertRelation: PRevert,
+  SelfDestructRelation: PSelfdestruct
+} = require("../src/node/plant");
+
+const { relationsToMatch, verifyTypes } = require("./helper");
+
+const txSelfDestruct = require("./seed/self-destruct");
+
+describe("Self Destruct", () => {
+  let umlCommands, actors, state;
+
+  beforeEach(() => {
+    umlCommands = [];
+    actors = new Actors({ shortParticipantNames: false });
+    state = { revertSource: [], deactivations: [], address: [] };
+  });
+
+  test("selfestruct", () => {
+    visit(txSelfDestruct, null, umlCommands, actors, state);
+    expect(umlCommands.length).toBe(4);
+    expect(
+      relationsToMatch(umlCommands, [
+        PCall,
+        PCall,
+        PReturn,
+        PSelfdestruct
+      ])
+    ).toBe(true);
+
+    const [call1, call2, _ , destruct_ ] = umlCommands;
+
+    expect(call1.value.toString()).toBe("0");  // no eth value
+    expect(call1.message).toBe("constructor");  
+    expect(call1.destination).toBe("StacktraceTest_01");
+    verifyTypes(call1.parameters[0], { name: "x", type: "uint", value: "1" });
+
+    expect(call2.value.toString()).toBe("0"); // no eth value
+    expect(call2.message).toBe("constructor"); // no eth value
+    expect(call2.destination).toBe("PakSau_01");
+    expect(call2.parameters.length).toBe(0);
+
+    // it should revert the entire transaction
+    expect(destruct_.source).toBe("StacktraceTest_01");
+    expect(destruct_.destination).toBe("EOA");
+    expect(destruct_.arrow).toBe("x-[#green]->");
+    expect(destruct_.returnValues).toBe(undefined); // there should be no return values
+
+  });
+
+});
