6000cb8d9c1f5ea789cbb3585120c6497e0b1df9
Amal Sudama <cds.sudama@gmail.com>
2021-03-03 08:52:04 -0500

    Apply Format

--- test/self-destruct.test.js
+++ test/self-destruct.test.js
@@ -24,20 +24,14 @@ describe("Self Destruct", () => {
     visit(txSelfDestruct, null, umlCommands, actors, state);
     expect(umlCommands.length).toBe(4);
     expect(
-      relationsToMatch(umlCommands, [
-        PCall,
-        PCall,
-        PReturn,
-        PSelfdestruct
-      ])
+      relationsToMatch(umlCommands, [PCall, PCall, PReturn, PSelfdestruct])
     ).toBe(true);
 
-    const [call1, call2, _ , destruct_ ] = umlCommands;
     // ignore 3rd command
     const [call1, call2, , destruct_] = umlCommands;
 
-    expect(call1.value.toString()).toBe("0");  // no eth value
-    expect(call1.message).toBe("constructor");  
+    expect(call1.value.toString()).toBe("0"); // no eth value
+    expect(call1.message).toBe("constructor");
     expect(call1.destination).toBe("StacktraceTest_01");
     verifyTypes(call1.parameters[0], { name: "x", type: "uint", value: "1" });
 
@@ -51,7 +45,5 @@ describe("Self Destruct", () => {
     expect(destruct_.destination).toBe("EOA");
     expect(destruct_.arrow).toBe("x-[#green]->");
     expect(destruct_.returnValues).toBe(undefined); // there should be no return values
-
   });
-
 });
